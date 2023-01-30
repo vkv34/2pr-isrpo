67fc5436ecf063dece9b9f9f09d75244643aa4c3
vkv34 <97910233+vkv34@users.noreply.github.com>
2023-01-30 11:14:07 +0300

    2

--- 2.py
+++ 2.py
@@ -1,31 +1,21 @@
 year=int(input("Введите год: "))
 sum = 0
 
-
 def numSum(num: int)-> int:
   s = 0
-  for item in str(num):
+  string = ""
+  for i in range(num+1):
+    string += str(i)
+  for item in string:
     s+=int(item)
   return s
- 
-if(year%4==0 and (year%100!=0 or year%400==0)):
-  visocosny = True
-else: 
-  visocosny = False
 
-for i in range(1,12):
-  if(i == 2):
-    if(visocosny):
-      for j in range(1, 29):
-        sum += numSum(j)
-    else:
-      for j in range(1, 28):
-        sum += numSum(j)
-  if(i % 2 != 0):
-    for j in range(1, 31):
-      sum += numSum(j)
-  elif(i % 2 == 0):
-    for j in range(1, 30):
-      sum += numSum(j)
-    
+if(year%4==0 and year%100!=0 or year%400==0):
+  daysInMonth= [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+else: 
+  daysInMonth= [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+for i in daysInMonth:
+  sum+=numSum(i)
+
 print(sum)
